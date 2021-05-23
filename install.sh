7171c5d09dd41863351d1594035507b3b8b2e306
BOT-KIRITO <84589268+BOT-KIRITO@users.noreply.github.com>
2021-05-23 08:38:24 -0700

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,16 +1,9 @@
 #!/usr/bin/bash
 
-pkg update -y
-pkg upgrade -y
-pkg install wget -y
-pkg install ffmpeg -y
-pkg install nodejs -y
-pkg install npm -y
-pkg install tesseract -y
-apt install nmap -y
-npm i
+apt-get update
+apt-get upgrade
+apt-get install -y nodejs libwebp ffmpeg wget tesseract
 wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
 npm install
-npm audit fix
 
-echo "[*] SUKSES BRO SILAHKAN KETIK \"npm start\" JIKA ADA YANG ERROR SILAHKAN INSTALL MANUAL"
+echo "[*] All dependencies have been installed, please run the command \"npm start\" to immediately start the script"
